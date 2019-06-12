1d7aec5fe244bf697814ca84e0ef6d84f345958b
Zolton <gmzesq@gmail.com>
2019-06-12 15:28:27 -0400

    Added notes for tomorrow's project

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -11,27 +11,36 @@ export default class FriendList extends React.Component {
     axios
       .get("http://localhost:5000/friends")
       .then(res => this.setState({friends: res.data}) )
-      
-      
       .catch(rej => console.log(rej));
   }
+  // axios fetches data from API, setsState, then forces a re-render of the page
+  // Alternate - this.state.friends is what would be passed down to components
+  // if friends were split into more pieces
+  /* <Route 
+    path="friend"
+    render={props => {
+      <Friend 
+      {...props} 
+      friend={this.friends.map(friend=>return friend)} 
+      />
+    }} 
+    Map would just create a new card/component for each friend, change below to just use <Friend />
+    while the Friend card/function would just use props.name, props.age, etc to list everything out
+    */
 
   render() {
     console.log("Hello from friendlist");
     return (
       <>
-        <h3>hello from friendList!!</h3>
         {/* Map over array, spit out each object as its own item, */}
         {this.state.friends.map(friend =>{
           return (
             <>
-            
             {/* Display info in each object */}
             <p>Friend info:</p>
             <div>Name: {friend.name}</div>
             <div>Age: {friend.age}</div>
             <div>Email: {friend.email}</div>
-            
             </>
           )}
           )}
@@ -47,6 +56,7 @@ export default class FriendList extends React.Component {
             // take existing array, pour it into a new array, add newFriend at the end, then reset the form value to zero
             // multipel fields require multiple inputs, just aggregate changes in onSubmit/addNewFriend function
             />
+            <button>Submit</button>
       </>
     );
   }
